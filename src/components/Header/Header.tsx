f44899c0b137c85a7d6e9697add3e07de20b29e6
Vadim Novikov <alinsbomb@yandex.ru>
2024-03-03 12:53:49 +0300

    add burger and some adaptive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
-import './Header.scss'
+import "./Header.scss";
+import { useState } from "react";
 
 const Header = () => {
+  const [burgerActive, setBurgerActive] = useState(false)
+
   return (
     <div className="container">
       <div className="header">
         <div className="header__logo logo">
           <div className="logo__inner"></div>
         </div>
-        <nav className="header__nav">
+        <nav className={burgerActive ? "header__nav-burger" : "header__nav"}>
           <ul className="header__items">
             <li className="header__item">
               <Link to="/">Услуги</Link>
@@ -26,10 +29,13 @@ const Header = () => {
         </nav>
         <div className="header__rightBar rightBar">
           <div className="rightBar__inner"></div>
+          <div className="nav-btn" onClick={() => setBurgerActive(item => !item)}>
+            <span />
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,11 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
+      <div className='super-wrapper'>
         <Header/>
           <Outlet/>
-        <Footer/>
+      </div>
+      <Footer/>
     </>
     
   )
